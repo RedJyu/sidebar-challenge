c6133799c14235f439efc0bd04fb1f960f36f1ba
RedJyu <piotr.stezycki@protonmai.com>
2023-10-12 13:31:18 +0200

    modal pre css

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,16 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './Context';
 function Modal() {
-  return <div>Modal</div>;
+  const { isModalOpen, closeModal } = useGlobalContext();
+  return (
+    <div className='modal-overlay'>
+      <div className='modal-container'>
+        <h3>modal test</h3>
+        <button className='close-modal-btn' onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
 }
 export default Modal;
