9604260be43e596758bc75c3f3a7837127a036f2
RedJyu <piotr.stezycki@protonmai.com>
2023-10-12 13:24:08 +0200

    home css

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,17 @@
+import { useGlobalContext } from './Context';
+import { FaBars } from 'react-icons/fa';
+
 function Home() {
-  return <div>Home</div>;
+  const { openSidebar, openModal } = useGlobalContext();
+  return (
+    <main>
+      <button onClick={openSidebar} className='sidebar-toggle'>
+        <FaBars />
+      </button>
+      <button onClick={openModal} className='btn'>
+        show modal
+      </button>
+    </main>
+  );
 }
 export default Home;
